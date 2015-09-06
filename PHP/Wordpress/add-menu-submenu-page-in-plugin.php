c88052ec34306aaee4e13583b3f13d89fba865ab
unknown <skbmoon.work@gmail.com>
2015-09-07 04:47:32 +0600

    Added the class for main menu

--- PHP/Wordpress/add-menu-submenu-page-in-plugin.php
+++ PHP/Wordpress/add-menu-submenu-page-in-plugin.php
@@ -38,7 +38,7 @@ Class Menu_page{
 		//add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
 		//function name cane be 'myplugin/plugin-admin-page.php' in which case the page display code should
 		//be at myplugin/myplugin-admin-page.php file
-		add_menu_page('Custom Page Title', 'Custom Menu Title', 'activate_plugins', 'custom-menu-slug', array($this, 'function_that_will_be_used'), plugin_url('myplugin/images/icon.png'), 6  );
+		add_menu_page('Custom Page Title', 'Custom Menu Title', 'activate_plugins', 'custom-menu-slug', array(new Main_menu(), 'process'), plugin_url('myplugin/images/icon.png'), 6  );
 		//add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function );
 		add_submenu_page('custom-menu-slug', 'Custom Submenu Page title', 'Custom Sub Menu Title', 'activate_plugins', 'custom-submenu-slug-or-same-as-parent', array($this, 'function_for_submenu') );
 	}
